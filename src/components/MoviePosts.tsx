1442512ee73b14eb0d5d66b04d4e63676d95b342
25rae25 <whdudfo0124@gmail.com>
2024-03-14 22:09:39 +0900

    tailwind 수정

--- src/components/MoviePosts.tsx
+++ src/components/MoviePosts.tsx
@@ -1,12 +1,12 @@
-import { getAllPosts } from "@/service/posts";
+import { getFeaturedPosts } from "@/service/posts";
 import PostsGrid from "./PostsGrid";
 
 export default async function MoviePosts() {
 
-	const posts = await getAllPosts()
+	const posts = await getFeaturedPosts()
 	return(
 		<section>
-			<h2>Movice Posts</h2>
+			<h2 className="text-2xl font-bold my-2">Movice Posts</h2>
 			<PostsGrid posts={posts} />
 		</section>
 	)
