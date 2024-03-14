f3372844c133488ab379a805209146e1f9f26527
25rae25 <whdudfo0124@gmail.com>
2024-03-14 23:35:18 +0900

    posts image 교체

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,9 +14,11 @@ export default function Hero() {
 			 priority />
 			<h2 className="text-3xl font-bold mt-2">{"안녕하세요 조형래입니다"}</h2>
 			<h3 className="text-xl font-semibold">영화 촬영감독</h3>
-			<p>중앙대학교 영화학과 학사</p>
+			<p className="mt-2">제32회 부일영화상 촬영상 - 콘크리트 유토피아</p>
+			<p className="mt-2">제38회 청룡영화상 촬영조명상 - 불한당:나쁜 놈들의 세상</p>
+			<p className="my-2">제38회 청룡영화상 조명상 - 불한당:나쁜 놈들의 세상</p>
 			<Link href='/content'>
-				<button className="bg-yellow-500 font-bold rounded-xl py-1 px-4 mt-2">Contact Me</button>
+				<button className="bg-yellow-500 font-bold rounded-xl py-1 px-4 my-4">Contact Me</button>
 			</Link>
 		</section>
 	)
