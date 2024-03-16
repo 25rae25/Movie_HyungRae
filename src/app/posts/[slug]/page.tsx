e230f4c85dee4fdc204b7b45b0681bd8ad6521df
25rae25 <whdudfo0124@gmail.com>
2024-03-16 21:45:33 +0900

    prev,next css 수정

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import AdjacentPostCard from "@/components/AdjacentPostCard";
 import MarkdownViewer from "@/components/MarkdownViewer";
 import PostContent from "@/components/PostContent";
 import { getPostData } from "@/service/posts";
@@ -22,9 +23,9 @@ export default async function PostPage({params: { slug } }:Props) {
 			alt={title} width={760} height={420}
 			 />
 			<PostContent post={post} />
-			<section>
-				{prev && <p>{prev.title}</p>}
-				{next && <p>{next.title}</p>}
+			<section className="flex shadow-md">
+				{prev && <AdjacentPostCard post={prev} type='prev' />}
+				{next && <AdjacentPostCard post={next} type='next' />}
 			</section>
 		</article>
 	)
